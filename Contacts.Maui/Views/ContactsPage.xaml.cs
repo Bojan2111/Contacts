4827174ce2f5ce325087a11d18019fad6276768e
Bojan2111 <bojan.adzic@gmail.com>
2024-06-15 16:56:11 +0200

    Add search functionality

--- Contacts.Maui/Views/ContactsPage.xaml.cs
+++ Contacts.Maui/Views/ContactsPage.xaml.cs
@@ -15,6 +15,8 @@ public partial class ContactsPage : ContentPage
     {
         base.OnAppearing();
 
+        searchBar.Text = string.Empty;
+
         LoadContacts();
     }
 
@@ -52,5 +54,12 @@ public partial class ContactsPage : ContentPage
         ContactRepository.RemoveContact(contact.Id);
 
         LoadContacts();
+    }
+
+    private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        var contacts = new ObservableCollection<Contact>(ContactRepository.SearchContacts(((SearchBar)sender).Text));
+
+        listContacts.ItemsSource = contacts;
     }
 }
